77f1deecaa8ea56db000c924de48d1d124a258bf
John Wolf <info@swisswebstudio.ch>
2015-07-27 17:13:58 +0200

    composer update

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
 
         Aws\Laravel\AwsServiceProvider::class,
         Aglipanci\Interspire\InterspireServiceProvider::class,
+        Spatie\ResponseCache\ResponseCacheServiceProvider::class,
 
     ],
 
